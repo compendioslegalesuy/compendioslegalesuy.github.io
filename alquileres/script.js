b45da423d6e5ce75a53af54e2be14e012acdeb2a
María Eugenia Szwedowski <47260556+compendioslegalesuy@users.noreply.github.com>
2022-12-29 00:20:35 -0300

    Add files via upload

---
+++ alquileres/script.js
@@ -0,0 +1,71 @@
+const menuToggle = document.querySelector('.menuToggle')
+const navigation = document.querySelector('.navigation')
+
+menuToggle.onclick = function(){
+    navigation.classList.toggle('open')
+}
+
+const list = document.querySelectorAll('.list')
+function activarLink(){
+    list.forEach((item)=>
+        item.classList.remove('active'))
+        this.classList.add('active')
+}
+
+list.forEach((item)=>
+item.addEventListener('click',activarLink)
+)
+
+
+// Typeado
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+};
